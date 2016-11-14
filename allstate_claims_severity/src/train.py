de05c6e1b6f9ae6b57b20e521e078c72414d5eda
Richard Wong <rk2wong@uwaterloo.ca>
2016-11-14 14:16:03 +0800

    Add loss transform.

--- allstate_claims_severity/src/train.py
+++ allstate_claims_severity/src/train.py
@@ -3,10 +3,25 @@ import xgb
 import xgboost
 
 
-def train(train_path):
-    params = {
+def transform(df_col, logshift=1):
+    return np.log(df_col + logshift)
 
-    }
-    trainset = xgboost.DMatrix(train_path)
+
+def inverse(df_col, logshift=1):
+    return np.exp(df_col) - logshift
+
+
+def train(train_path, label_path, params={}):
+    label = transform(pd.read_csv(label_path))
+    trainset = xgboost.DMatrix(train_path, label=label)
     booster = xgboost.train(params=params, dtrain=trainset)
     return booster
+
+
+def predict(booster, test_path, id_path):
+    testset = xgboost.DMatrix(test_path)
+    ids = pd.read_csv(id_path)
+    result = inverse(pd.DataFrame(booster.predict(testset), columns=["loss"]))
+    pd.concat((ids, result), axis=1).to_csv("../output/submission.csv",
+                                            index=False)
+    print("Output written to ../output/submission.csv")
