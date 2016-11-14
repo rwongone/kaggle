f39f884d72ae5d6225b15e4327f30c47c2e9501a
Richard Wong <rk2wong@uwaterloo.ca>
2016-11-14 14:17:53 +0800

    Defaults.

--- allstate_claims_severity/src/train.py
+++ allstate_claims_severity/src/train.py
@@ -3,6 +3,11 @@ import xgb
 import xgboost
 
 
+TRAIN_PATH = "../var/train.onehot"
+TEST_PATH = "../var/test.onehot"
+ID_PATH = "../var/id.csv"
+LABEL_PATH = "../var/loss.csv"
+
 def transform(df_col, logshift=1):
     return np.log(df_col + logshift)
 
@@ -11,14 +16,14 @@ def inverse(df_col, logshift=1):
     return np.exp(df_col) - logshift
 
 
-def train(train_path, label_path, params={}):
+def train(train_path=TRAIN_PATH, label_path=LABEL_PATH, params={}):
     label = transform(pd.read_csv(label_path))
     trainset = xgboost.DMatrix(train_path, label=label)
     booster = xgboost.train(params=params, dtrain=trainset)
     return booster
 
 
-def predict(booster, test_path, id_path):
+def predict(booster, test_path=TEST_PATH, id_path=ID_PATH):
     testset = xgboost.DMatrix(test_path)
     ids = pd.read_csv(id_path)
     result = inverse(pd.DataFrame(booster.predict(testset), columns=["loss"]))
